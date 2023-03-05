8436fdf9caa85e18eff5d68621baf8a9c112d035
MolnKriszt <krisz200601@gmail.com>
2023-03-05 11:18:02 +0100

    env

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
-const express = require('express')
-const app = express()
+require("dotenv").config();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const express = require('express');
+const mysql = require("mysql");
+const app = express();
 
-app.listen(3000)
+const pool = require("./config/database.js");
+
+app.get("/", (req, res) => {
+  
+  pool.getConnection(function (error, connection) {
+    if (error) {
+      console.log("server error");
+      return;
+    }
+    const sql = "SELECT * FROM players";
+    connection.query(sql, (error, results, fields) => {
+      if (error) {
+        console.log("sql error");
+        return;
+      }
+      res.send(results);
+    });
+    connection.release();
+  });
+});
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Data server, listen port: ${process.env.APP_PORT}`);
+});
